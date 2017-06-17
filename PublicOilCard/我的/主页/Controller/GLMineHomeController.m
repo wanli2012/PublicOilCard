4a0a2aa967c381f8d2ae550f228120170c7239b5
龚磊 <1029235277@qq.com>
2017-06-17 18:26:39 +0800

    no message

--- PublicOilCard/我的/主页/Controller/GLMineHomeController.m
+++ PublicOilCard/我的/主页/Controller/GLMineHomeController.m
@@ -26,7 +26,7 @@
 {
     //假数据源
     NSArray *_keyArr;
-    NSArray *_vlaueArr;
+    NSArray *_imageArr;
     
     GLMine_HeaderView *_header;
 }
@@ -46,7 +46,7 @@ static NSString *headerID = @"GLMine_HeaderView";
     [usermodelachivar achive];
 
     _keyArr = @[@"收藏",@"订单",@"升级管理",@"推荐"];
-   
+    _imageArr = @[@"收藏",@"订单",@"升级管理",@"推荐"];
     [self.view addSubview:self.collectionV];
     //注册头视图
     [self.collectionV registerClass:[GLMine_HeaderView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:headerID];
@@ -130,6 +130,7 @@ static NSString *headerID = @"GLMine_HeaderView";
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     GLMine_collectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellID forIndexPath:indexPath];
     cell.titleLabel.text = _keyArr[indexPath.row];
+    cell.picImageV.image = [UIImage imageNamed:_imageArr[indexPath.row]];
     return cell;
 }
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
