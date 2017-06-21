ccf35d01d7f5105627b010d9bad16389db808a4f
龚磊 <1029235277@qq.com>
2017-06-21 16:57:19 +0800

    no message

--- PublicOilCard/我的/个人信息/View/GLMine_PersonInfoCell.m
+++ PublicOilCard/我的/个人信息/View/GLMine_PersonInfoCell.m
@@ -2,14 +2,13 @@
 //  GLMine_PersonInfoCell.m
 //  PublicOilCard
 //
-//  Created by 龚磊 on 2017/6/15.
+//  Created by 龚磊 on 2017/6/21.
 //  Copyright © 2017年 三君科技有限公司. All rights reserved.
 //
 
 #import "GLMine_PersonInfoCell.h"
 
-@interface GLMine_PersonInfoCell ()
-
+@interface GLMine_PersonInfoCell()<UITextFieldDelegate>
 
 @end
 
@@ -19,10 +18,38 @@
     [super awakeFromNib];
     // Initialization code
 }
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+ 
+    
+    return YES;
+}
 
-- (IBAction)clickImageV:(id)sender {
+-(void)textFieldDidEndEditing:(UITextField *)textField{
+    
+    if (self.returnEditing) {
+        self.returnEditing(textField.text , self.index);
+    }
     
 }
 
-
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    
+//    if ([[UserModel defaultUser].usrtype isEqualToString:Retailer] && [[UserModel defaultUser].AudiThrough isEqualToString:@"1"]) {
+//        
+//        if (self.index == 5) {
+//            for(int i=0; i< [string length];i++){
+//                
+//                int a = [string characterAtIndex:i];
+//                
+//                if( a >= 0x4e00 && a <= 0x9fff)
+//                    
+//                    return NO;
+//            }
+//        }
+//        
+//    }
+    
+    return YES;
+    
+}
 @end
