bdcf609c655cc3f7a69ac36729fd8dd64849e79d
四川三君科技有限公司 <www.448128835@qq.com>
2017-07-09 18:03:40 +0800

    no message

---
+++ PublicOilCard/支付页面/view/LBPayFooterView.m
@@ -0,0 +1,21 @@
+//
+//  LBPayFooterView.m
+//  PublicOilCard
+//
+//  Created by 四川三君科技有限公司 on 2017/7/9.
+//  Copyright © 2017年 三君科技有限公司. All rights reserved.
+//
+
+#import "LBPayFooterView.h"
+
+@implementation LBPayFooterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
